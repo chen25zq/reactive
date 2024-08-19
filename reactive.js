f3203ba8b03cb01c1d78263bea9f7afa68bde6d5
chen25zq <chenzq_t@163.com>
2024-08-19 15:56:11 +0800

    加深 reactive 的判断，是否为对象且是否已经代理过

--- reactive.js
+++ reactive.js
@@ -1,9 +1,25 @@
 // 入口文件，提供 reactive API，接收一个对象，返回一个代理对象
 
 import handlers from './handlers/index.js';
+import { isObject } from './utils.js';
+
+// 映射表: 用于存储原始对象和代理对象之间的映射关系
+const proxyMap = new WeakMap();
 
 // 将对象转化成Proxy对象
 export function reactive(target) {
+    // 判断target是否是一个对象
+    if (!isObject(target)) {
+        return target;
+    }
+
+    // 如果target本身就是一个Proxy对象，说明已经被代理过，则直接返回之前代理对象
+    if (proxyMap.has(target)) {
+        return proxyMap.get(target);
+    }
+
     const proxy = new Proxy(target, handlers);
+    proxyMap.set(target, proxy);
+    
     return proxy;
 }
